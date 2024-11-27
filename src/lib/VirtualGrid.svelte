12262e4e7b4852134445acf537abf0296de3f64f
KMB <mcdba@mail.ru>
2024-11-27 13:53:14 +0300

    add footer header in grid

--- src/lib/VirtualGrid.svelte
+++ src/lib/VirtualGrid.svelte
@@ -2,12 +2,14 @@
   import VirtualScroll from "./VirtualScroll.svelte";
   type Props = {
     child?: import("svelte").Snippet<[any]>;
+    header?: import("svelte").Snippet;
+    footer?: import("svelte").Snippet;
     data: any[];
     key?: string;
     columns: number;
     keeps?: number;
   };
-  let { data, key, child, columns, keeps }: Props = $props();
+  let { data, key, child, columns, keeps, footer, header }: Props = $props();
   // Function to chunk the array into groups of specified size
   const chunkArray = (array: any[], chunkSize: number) => {
     const result = [];
@@ -21,7 +23,14 @@
   const gridData = $state(chunkArray(data, columns));
 </script>
 
-<VirtualScroll data={gridData} {key} {keeps} classItem="grid grid-cols-3 gap-4">
+<VirtualScroll
+  data={gridData}
+  {key}
+  {keeps}
+  classItem="grid grid-cols-3 gap-4"
+  {footer}
+  {header}
+>
   {#snippet children({ data })}
     {#each data.data as item}
       {@render child?.({ item })}
--- src/routes/sample_grid2/+page.svelte
+++ src/routes/sample_grid2/+page.svelte
@@ -21,6 +21,13 @@
 <!-- <pre>{JSON.stringify(data, null, 2)}</pre> -->
 <div class="h-[500px]">
   <VirtualGrid {data} key="key" columns={3} keeps={8}>
+    {#snippet header()}
+      <div
+        class="mb-4 rounded-xl border border-purple-300 bg-purple-200 p-4 text-center text-xl font-bold"
+      >
+        This is a header snippet
+      </div>
+    {/snippet}
     {#snippet child({ item })}
       <div
         class="mb-4 overflow-hidden rounded-lg border border-slate-300 bg-slate-200 p-2 shadow-lg"
@@ -32,6 +39,13 @@
           {item.name}
         </h1>
         <p class="text-4xl font-bold">{item.price}</p>
+      </div>
+    {/snippet}
+    {#snippet footer()}
+      <div
+        class="rounded-xl border border-teal-300 bg-teal-200 p-4 text-center text-xl font-bold"
+      >
+        This is a footer snippet
       </div>
     {/snippet}
   </VirtualGrid>
